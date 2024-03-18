b1572e899147bb7322c15d38ce85e4de94901ddc
Scr34m1nG <adyatmaruliff@gmail.com>
2024-03-18 11:47:42 +0700

    update data class product request

--- src/main/kotlin/com/belajar/kotlinrestfulapi/model/ProductResponse.kt
+++ src/main/kotlin/com/belajar/kotlinrestfulapi/model/ProductResponse.kt
@@ -1,3 +1,12 @@
 package com.belajar.kotlinrestfulapi.model
 
-data class ProductResponse()
+import java.util.Date
+
+data class ProductResponse(
+    val id: String,
+    val name: String,
+    val price: Long,
+    val quantity: Int,
+    val createdAt: Date,
+    val updatedAt: Date
+)
