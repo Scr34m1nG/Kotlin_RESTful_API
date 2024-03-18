c5d939340cd9ca75c3a3c8d1c00258a689476b1d
Scr34m1nG <adyatmaruliff@gmail.com>
2024-03-18 15:01:00 +0700

    create update function

--- src/main/kotlin/com/belajar/kotlinrestfulapi/service/ProductService.kt
+++ src/main/kotlin/com/belajar/kotlinrestfulapi/service/ProductService.kt
@@ -2,11 +2,14 @@ package com.belajar.kotlinrestfulapi.service
 
 import com.belajar.kotlinrestfulapi.model.CreateProductRequest
 import com.belajar.kotlinrestfulapi.model.ProductResponse
+import com.belajar.kotlinrestfulapi.model.UpdateProductRequest
 
 interface ProductService {
 
     fun create(createProductRequest: CreateProductRequest): ProductResponse
 
     fun get(id: String): ProductResponse
+
+    fun update(id: String, updateProductRequest: UpdateProductRequest): ProductResponse
 
 }
