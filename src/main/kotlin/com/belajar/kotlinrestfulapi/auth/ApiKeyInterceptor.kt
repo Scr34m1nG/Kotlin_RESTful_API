7283d43dccd3581ebdda3695449a9e876f3e0984
Scr34m1nG <adyatmaruliff@gmail.com>
2024-03-18 17:01:38 +0700

    create api key interceptor

---
+++ src/main/kotlin/com/belajar/kotlinrestfulapi/auth/ApiKeyInterceptor.kt
@@ -0,0 +1,31 @@
+package com.belajar.kotlinrestfulapi.auth
+
+import com.belajar.kotlinrestfulapi.error.UnauthorizedException
+import com.belajar.kotlinrestfulapi.repository.ApiKeyRepository
+import org.springframework.stereotype.Component
+import org.springframework.ui.ModelMap
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.context.request.WebRequestInterceptor
+import java.lang.Exception
+
+@Component
+class ApiKeyInterceptor(val apiKeyRepository: ApiKeyRepository) : WebRequestInterceptor {
+    override fun preHandle(request: WebRequest) {
+        val apiKey = request.getHeader("X-Api-Key")
+        if (apiKey == null){
+            throw UnauthorizedException()
+        }
+
+        if (!apiKeyRepository.existsById(apiKey)){
+            throw UnauthorizedException()
+        }
+    }
+
+    override fun postHandle(request: WebRequest, model: ModelMap?) {
+
+    }
+
+    override fun afterCompletion(request: WebRequest, ex: Exception?) {
+
+    }
+}
