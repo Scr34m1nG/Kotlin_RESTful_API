5f386311bde505237bd1659568cccec929d4e2be
Scr34m1nG <adyatmaruliff@gmail.com>
2024-03-18 16:49:19 +0700

    create API key configuration

---
+++ src/main/kotlin/com/belajar/kotlinrestfulapi/config/ApiKeyConfiguration.kt
@@ -0,0 +1,21 @@
+package com.belajar.kotlinrestfulapi.config
+
+import com.belajar.kotlinrestfulapi.entity.ApiKey
+import com.belajar.kotlinrestfulapi.repository.ApiKeyRepository
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class ApiKeyConfiguration(val apiKeyRepository: ApiKeyRepository) : ApplicationRunner {
+
+    val apiKey = "SECRET"
+
+    override fun run(args: ApplicationArguments?) {
+        if (!apiKeyRepository.existsById(apiKey)){
+            val key = ApiKey(id = apiKey)
+            apiKeyRepository.save(key)
+        }
+    }
+
+}
