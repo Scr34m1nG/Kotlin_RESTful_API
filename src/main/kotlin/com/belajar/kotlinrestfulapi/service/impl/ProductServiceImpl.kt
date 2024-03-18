cd271939d5a25a6de58980702ba8fb5b343fe29c
Scr34m1nG <adyatmaruliff@gmail.com>
2024-03-18 12:01:45 +0700

    created product service implementation

---
+++ src/main/kotlin/com/belajar/kotlinrestfulapi/service/impl/ProductServiceImpl.kt
@@ -0,0 +1,34 @@
+package com.belajar.kotlinrestfulapi.service.impl
+
+import com.belajar.kotlinrestfulapi.entity.Product
+import com.belajar.kotlinrestfulapi.model.CreateProductRequest
+import com.belajar.kotlinrestfulapi.model.ProductResponse
+import com.belajar.kotlinrestfulapi.repository.ProductRepository
+import com.belajar.kotlinrestfulapi.service.ProductService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class ProductServiceImpl(val productRepository: ProductRepository) : ProductService{
+    override fun create(createProductRequest: CreateProductRequest): ProductResponse {
+        val product = Product(
+            id = createProductRequest.id,
+            name = createProductRequest.name,
+            price = createProductRequest.price,
+            quantity = createProductRequest.quantity,
+            createdAt = Date(),
+            updatedAt = null
+        )
+
+        productRepository.save(product)
+
+        return ProductResponse(
+            id = product.id,
+            name = product.name,
+            price = product.price,
+            quantity = product.quantity,
+            createdAt = product.createdAt,
+            updatedAt = product.updatedAt
+        )
+    }
+}
