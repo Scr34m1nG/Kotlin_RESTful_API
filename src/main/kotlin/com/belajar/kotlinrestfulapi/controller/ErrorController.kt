7c5330a871fa94f4adac31420adab143c84dd718
Scr34m1nG <adyatmaruliff@gmail.com>
2024-03-18 12:53:20 +0700

    create error controller and validationHandler

---
+++ src/main/kotlin/com/belajar/kotlinrestfulapi/controller/ErrorController.kt
@@ -0,0 +1,19 @@
+package com.belajar.kotlinrestfulapi.controller
+
+import com.belajar.kotlinrestfulapi.model.WebResponse
+import jakarta.validation.ConstraintViolationException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ErrorController {
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun validationHandler(constraintViolationException: ConstraintViolationException): WebResponse<String>{
+        return WebResponse(
+            code = 400,
+            status = "BAD REQUEST",
+            data = constraintViolationException.message!!
+        )
+    }
+}
